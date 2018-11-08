b8c4a86fa94131dcd6f83d57d7c0b36304049702
Theodore Wu <twu00@stuy.edu>
2018-11-07 20:20:33 -0500

    made the private method .copy() so .addWordHorizontal/Vertical can return the old list when the word cannot be added. (also made all the methods)

--- Driver.java
+++ Driver.java
@@ -1,1 +1,18 @@
-
+public class Driver{
+	public static void main(String[] args){
+		WordSearch one = new WordSearch(10,10);
+		System.out.println(one);
+		System.out.println(one.addWordHorizontal("hello",0,4));
+		System.out.println(one);
+		System.out.println(one.addWordHorizontal("oS",0,7));
+		System.out.println(one);
+		System.out.println(one.addWordHorizontal("oS",0,8));
+		System.out.println(one);
+		System.out.println("*******************************");
+		System.out.println(one.addWordVertical("elloHay",0,5));
+		System.out.println(one);
+		System.out.println(one.addWordVertical("LosCabos", 0, 7));
+		System.out.println(one);
+		
+	}
+}
--- WordSearch.java
+++ WordSearch.java
@@ -1,1 +1,98 @@
+public class WordSearch{
+    private char[][]data;
 
+    /**Initialize the grid to the size specified 
+     *and fill all of the positions with '_'
+     *@param row is the starting height of the WordSearch
+     *@param col is the starting width of the WordSearch
+     */
+    public WordSearch(int rows,int cols){
+    	data = new char[rows][cols];
+    	clear();
+    }
+
+    /**Set all values in the WordSearch to underscores'_'*/
+    private void clear(){
+    	for (int i = 0; i < data.length; i++){
+    		for (int e = 0; e < data[i].length; e++){
+    			data[i][e] = '_';
+    		}
+    	}
+    }
+
+    /**Each row is a new line, there is a space between each letter
+     *@return a String with each character separated by spaces, and rows
+     *separated by newlines.
+     */
+    public String toString(){
+    	String out = "";
+    	for (int i = 0; i < data.length; i++){
+    		for (int e = 0; e < data[i].length; e++){
+    			out += data[i][e] + " ";
+    		}
+    		out += "\n";
+    	}
+    	return out;
+    }
+
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from left to right, must fit on the WordGrid, and must
+     *have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     * or there are overlapping letters that do not match, then false is returned 
+     * and the board is NOT modified.
+     */
+    public boolean addWordHorizontal(String word,int row, int col){
+    	if (data[row].length - col < word.length()) return false;
+    	char[][] old = copy();
+
+    	for (int i = col, e = 0; e < word.length(); i++, e++){
+    		if (data[row][i] == ('_')) {data[row][i] = word.charAt(e);}
+    		if (data[row][i] != word.charAt(e)) { 
+    			data = old;
+    			return false;}
+    	}
+    	return true;
+    }
+
+    /* *@return a copy of the data[][] field 
+    */
+    private char[][] copy(){
+    	char[][] old = new char[data.length][data[0].length];
+    	for (int i = 0; i < data.length; i++){
+    		for (int e = 0; e < data[i].length; e++){
+    			old[i][e] = data[i][e];
+    		}
+    	}
+    	return old;
+    }
+
+   /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from top to bottom, must fit on the WordGrid, and must
+     *have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     *and the board is NOT modified.
+     */
+    public boolean addWordVertical(String word,int row, int col){
+    	if (data.length - row < word.length()) return false;
+    	char[][] old = copy();
+
+    	for (int i = row, e = 0; e < word.length(); i++, e++){
+    		if (data[i][col] == ('_')) {data[i][col] = word.charAt(e);}
+    		if (data[i][col] != word.charAt(e)) { 
+    			data = old;
+    			return false;}
+    	}
+    	return true;
+    }
+}
