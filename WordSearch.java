7f5da1d1ded7be2e1776be5e95c28107fcb179e3
Theodore Wu <twu00@stuy.edu>
2018-11-19 19:07:37 -0500

    made it so that many directions are tried for one word, I was under the impression only one direction was to be chosen per word. This is probably too late but the other one works just fine

--- WordSearch.java
+++ WordSearch.java
@@ -76,45 +76,51 @@ public class WordSearch{
     	int s = words.size();
     	for (int w = 0; w < s; w++){
 	    	String rWord = words.get(rand.nextInt(words.size()));
-	    	int rXin = 0, rYin = 0;
-	    	while (rXin == 0 & rYin == 0){
-		    		rXin = rand.nextInt(3) - 1;
-		    		rYin = rand.nextInt(3) - 1;
-		    	}
-		    boolean[][] ps = new boolean[data.length][data[0].length];
-		    int wl = rWord.length();
-		    int rt, yt;
-	        if (rXin != -1) {rt = ps.length - (wl);}else {rt = (wl-1);}
-	        if (rYin != -1) {yt = ps[0].length - (wl);}else {yt = (wl-1);}
-	        int totalP = ps.length * ps[0].length;
-		    for (int x = 0; x < ps.length; x++){
-		    	for (int y = 0; y < ps[0].length; y++){
-		    		if (x*rXin > rt*rXin || y*rYin > yt*rYin) {
-		    			ps[x][y] = true;
-		    			--totalP;
-		    		}
-		    		if ((data[x][y] != rWord.charAt(0) && data[x][y] != '_') && !ps[x][y]) {
-		    			ps[x][y] = true;
-		    			--totalP;
-		    		}
-		    	}
-		    }
-	    	for (int i = 0; i < totalP; i++){
-		    	int rX = rand.nextInt(data.length); 
-		    	int rY = rand.nextInt(data[0].length);
-		    	while (ps[rX][rY]){
-		    		rX = rand.nextInt(data.length); 
-		    		rY = rand.nextInt(data[0].length);
-		    	}
-		    	ps[rX][rY] = true;
-		    	if (addWord(rX,rY,rWord,rXin, rYin)){
-		    		words.remove(rWord);
-    				addedWords.add(rWord);
-    				i = totalP;
-		    	}
-		    	if (i == totalP - 1) {words.remove(rWord);}
-			}
-	    }
+            boolean[][] directionTried = new boolean[3][3];
+            directionTried[1][1] = true;
+            for (int d = 0; d < 8; d++){
+    	    	int rXin = 0, rYin = 0;
+    	    	while (directionTried[rXin+1][rYin+1]){
+    		    		rXin = rand.nextInt(3) - 1;
+    		    		rYin = rand.nextInt(3) - 1;
+    		    	}
+                directionTried[rXin+1][rYin+1] = true;
+    		    boolean[][] ps = new boolean[data.length][data[0].length];
+    		    int wl = rWord.length();
+    		    int rt, yt;
+    	        if (rXin != -1) {rt = ps.length - (wl);}else {rt = (wl-1);}
+    	        if (rYin != -1) {yt = ps[0].length - (wl);}else {yt = (wl-1);}
+    	        int totalP = ps.length * ps[0].length;
+    		    for (int x = 0; x < ps.length; x++){
+    		    	for (int y = 0; y < ps[0].length; y++){
+    		    		if (x*rXin > rt*rXin || y*rYin > yt*rYin) {
+    		    			ps[x][y] = true;
+    		    			--totalP;
+    		    		}
+    		    		if ((data[x][y] != rWord.charAt(0) && data[x][y] != '_') && !ps[x][y]) {
+    		    			ps[x][y] = true;
+    		    			--totalP;
+    		    		}
+    		    	}
+    		    }
+    	    	for (int i = 0; i < totalP; i++){
+    		    	int rX = rand.nextInt(data.length); 
+    		    	int rY = rand.nextInt(data[0].length);
+    		    	while (ps[rX][rY]){
+    		    		rX = rand.nextInt(data.length); 
+    		    		rY = rand.nextInt(data[0].length);
+    		    	}
+    		    	ps[rX][rY] = true;
+    		    	if (addWord(rX,rY,rWord,rXin, rYin)){
+    		    		words.remove(rWord);
+        				addedWords.add(rWord);
+        				i = totalP;
+                        d = 8;
+    		    	}
+    		    	if (i == totalP - 1 && d == 7) {words.remove(rWord);}
+    			}
+    	    }
+        }
     	return true;
     }
     private boolean fillIn(){
@@ -140,6 +146,7 @@ public class WordSearch{
 	  		System.out.println(ws);
 	  	}
 	  	catch (Exception e){
+            //e.printStackTrace();
 	  		System.out.println("Wrong Input!!! \n************************\nFirst enter  a valid number of rows you would like in your word search.");
 	  		System.out.println("Next a valid number of columns, and then an existing .txt file containing the words you wish to find.");
 	  		System.out.println("If you have a valid seed (in the range 0 to 10000) enter it after the column. if you wish to see the answers type \"key\" last. Each input should be seperated by spaces");
